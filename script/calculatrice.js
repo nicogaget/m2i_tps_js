ed5d9f94a9c072c607b3dbb508a587ff761d2a51
Nicolas Gaget <n.gaget69@gmail.com>
2021-10-22 16:44:39 +0200

    calculatrice Ne fonctionne pas totalement

--- script/calculatrice.js
+++ script/calculatrice.js
@@ -1,26 +1,77 @@
-function addition(nbr1, nbr2) {
-  alert(+nbr1.value + +nbr2.value);
+function addition(tmp1, tmp2) {
+  return parseInt(tmp1) + parseInt(tmp2);
 }
 
-function soustraction(nbr1, nbr2) {
-  alert(+nbr1.value - +nbr2.value);
+function soustraction(ntmp1, tmp2br) {
+  return parseInt(tmp1) - parseInt(tmp2);
 }
 
-function multiplication(nbr1, nbr2) {
-  alert(+nbr1.value * +nbr2.value);
+function multiplication(tmp1, tmp2) {
+  return parseInt(tmp1) * parseInt(tmp2);
 }
 
-function division(nbr1, nbr2) {
-  if (+nbr2.value == 0) {
+function division(tmp1, tmp2) {
+  if (parseInt(tmp1.value == 0)) {
     alert("Division par 0 impossible");
   } else {
-    alert(+nbr1.value / +nbr2.value);
+    parseInt(tmp1) / parseInt(tmp2);
   }
 }
 function edit(int) {
   number = document.querySelector("#saisie");
-  number.placeholder = number.placeholder + int;
-  console.log(number);
+  number.placeholder = parseInt(number.placeholder + int);
+
+  console.log(number.placeholder);
 }
 
-function resultat() {}
+let ope = "";
+let tmp1 = "";
+let tmp2 = "";
+function resultat() {
+  saisie = document.querySelector("#saisie");
+  tmp2 = saisie.placeholder;
+  operation = document.querySelector("#ope");
+  ope = operation.placeholder;
+  console.log(tmp1, ope, tmp2);
+  resultat = document.querySelector("#resultat");
+
+  switch (ope) {
+    case "+":
+      resultat.placeholder = addition(tmp1, tmp2);
+      break;
+    case "-":
+      resultat.placeholder = soustraction(tmp1, tmp2);
+      break;
+    case "/":
+      resultat.placeholder = division(tmp1, tmp2);
+      break;
+    case "*":
+      resultat.placeholder = multiplication(tmp1, tmp2);
+      break;
+  }
+  operation.placeholder = "";
+  saisie.placeholder = "";
+}
+
+function operande(ope) {
+  console.log(ope);
+  saisie = document.querySelector("#saisie");
+  tmp1 = saisie.placeholder;
+  ope = ope;
+  operation = document.querySelector("#ope");
+  operation.placeholder = ope;
+  ope = operation.placeholder;
+
+  number.placeholder = "";
+}
+function reset() {
+  saisie = document.querySelector("#saisie");
+  operation = document.querySelector("#ope");
+  resultat = document.querySelector("#resultat");
+  operation.placeholder = "";
+  saisie.placeholder = "";
+  resultat.placeholder = "";
+  tmp1 = "";
+  tmp2 = "";
+  ope = "";
+}
